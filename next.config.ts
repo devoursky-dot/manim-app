278b355538156162a405da7c3b0454354ffeb310
lbwooo <devoursky@gmail.com>
2026-02-26 09:18:40 +0900

    error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
   webpack: (config, { isServer }) => {
     // 클라이언트 사이드(브라우저) 빌드 시 'fs' 모듈 무시
     if (!isServer) {
       config.resolve.fallback = {
+        ...config.resolve.fallback,
         fs: false,
       };
     }
